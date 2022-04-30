b8d5c61b8f3ad8cdebe782b5114aebd04bb1459e
You-Wen Luo <luorich115970@gmail.com>
2022-04-30 17:57:03 +0800

    Add day styles: Today is red, before and after this month is grey

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -4,13 +4,35 @@ import "./styles.css";
 import buildCalendar from "./build";
 
 export default function Calendar() {
-
     const [calendar, setCalendar] = useState([]);
     const [value, setValue] = useState(moment());
+    const firstDay = value.clone().startOf("month");
+    const lastDay = value.clone().endOf("month");
 
     useEffect(() => {
         setCalendar(buildCalendar(value));
     }, [value])
+
+    function isSelected(day){
+        return value.isSame(day, "day");
+    }
+
+    function notThisMonth(day){
+       return day.isBefore(firstDay, "day") || day.isAfter(lastDay, "day"); 
+    }
+    /* || day.isAfter(lastDay, "day")*/
+
+    function isToday(day){
+        return day.isSame(new Date(), "day");
+    }
+
+    function dayStyles(day){
+        if (notThisMonth(day)) return "notThisMonth"
+        if (isSelected(day)) return "selected"
+        if (isToday(day)) return "today"
+        return ""
+
+    }
 
     return (
         <div className="calendar">
@@ -18,7 +40,7 @@ export default function Calendar() {
                 <div>
                     {week.map((day) => (
                         <div className="day" onClick = {() => setValue(day)}>
-                            <div className={value.isSame(day, "day") ? "selected" : ""}>
+                            <div className={dayStyles(day)}>
                                 {day.format("D")}
                             </div>
                         </div>
