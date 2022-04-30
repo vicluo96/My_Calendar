9952edd7f85b0ce550d6f062ea90d2a24a628e1b
You-Wen Luo <luorich115970@gmail.com>
2022-04-30 15:12:01 +0800

    Define the start day and the end day of the current month

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import moment from "moment"
 
-export default function Calendar(){
-    return <div>Calendar place holder</div>;
+export default function Calendar() {
+    const value = moment()
+    const startDay = value.clone().startOf("month").startOf("week");
+    const endDay = value.clone().endOf("month").endOf("week").add(7, 'days');
+    const day = startDay.clone().subtract(1, "day");
+    const calendar = []
+    return (
+        <div>
+            {startDay.format("MM/DD")} - {endDay.format("MM/DD")}
+        </div>
+        );
 }
