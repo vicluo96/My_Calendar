e1c824847991fabd7725cfed49c91ca5617e376d
You-Wen Luo <luorich115970@gmail.com>
2022-05-01 12:32:01 +0800

    finish calendar without month and year picker

---
+++ README.md
@@ -0,0 +1,7 @@
+# Build a calendar component using ReactJS
+
+Hello world and happy hacking!
+
+This is the starter repo with the fonts, css, and directory structure you will need to follow along with INSERT FINAL SCREENCAST LINK HERE.
+
+To start, FORK this repository, clone your fork down, andfollow along with the screencast.
---
+++ src/app/calendar/monthPicker.jsx
@@ -0,0 +1,13 @@
+/*import React from "react";
+import "./styles.css";
+
+export default function buildMonth({value, setValue}) {
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    return ({months.map((m) => (
+        <div className="month">{m}</div>
+        ))}
+    );
+
+}*/
